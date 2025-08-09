8fc39337d0189fb42b71b1b3cbfe3c71a0f74eb5
Michael Preciado <96038300+michaelpreciado@users.noreply.github.com>
2025-08-09 16:53:05 -0700

    feat: add liquid glass scan button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import CameraFeed from './components/CameraFeed';
 import DetectionOverlay from './components/DetectionOverlay';
 import MoodCard from './components/MoodCard';
+import ScanButton from './components/ScanButton';
 
 const App: React.FC = () => {
   return (
     <main className="relative w-full h-dvh overflow-hidden select-none touch-none">
       <CameraFeed />
       <DetectionOverlay />
+      <ScanButton />
       <MoodCard />
     </main>
   );
